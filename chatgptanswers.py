1382136348be723f38f1aeb71107c83e6d04a546
Prateek Shekhar <sephi-22@hotmail.com>
2023-11-12 21:39:09 +0400

    Exclude specified files using .gitignore

--- chatgptanswers.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(word):
-    reversed_word = ''
-    for char in word:
-        reversed_word = char + reversed_word
-    return word == reversed_word
-
-# Example usage:
-result = is_palindrome("abba")
-print(result)  # Output: True
